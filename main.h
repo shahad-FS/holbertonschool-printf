21139f8e0245bc116053b3bd35f7d0cd37ec9e40
shahad-FS <alsaneea.s@gmail.com>
2025-09-29 06:19:42 +0000

    update function prototype

--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int _putstr(va_list args);
-int _putpct(va_list args);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_pct(va_list args);
 #endif
