87aa7ab6d2d83995916b99ba908bb6c6f5e902c7
omar-hail <donz.h.h@gmail.com>
2025-09-30 10:16:08 +0000

    update

--- main.h
+++ main.h
@@ -20,10 +20,5 @@ int print_S(va_list args);
 int print_hex_lower(va_list args);
 int print_hex_upper(va_list args);
 int print_pointer(va_list args);
-int print_reverse(va_list args);
-int _puts(char *str);
-int print_ptr(va_list args);
-int print_hex_address(unsigned long int num);
-int print_rev(va_list args);
 
 #endif
