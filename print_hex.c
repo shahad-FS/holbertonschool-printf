97b4d0ce40511c9a2e72ad6b23dfbf8fb4efd34f
shahad-FS <alsaneea.s@gmail.com>
2025-10-01 21:53:37 +0000

    update the code to handel width flag

--- print_hex.c
+++ print_hex.c
@@ -9,11 +9,11 @@ int print_hex(va_list args, format_flags_t *f, int uppercase)
     char offset = uppercase ? 'A' - 10 : 'a' - 10;
     int padding = 0;
 
-    /* Special case: n = 0 */
+  
     if (n == 0)
         buffer[i++] = '0';
 
-    /* Convert number to hex string in reverse */
+    
     while (n > 0)
     {
         int digit = n % 16;
@@ -23,25 +23,36 @@ int print_hex(va_list args, format_flags_t *f, int uppercase)
 
     num_len = i;
 
-    /* Calculate padding */
     if (f->width > num_len)
         padding = f->width - num_len;
 
-    /* Print zero padding if zero flag set and not left-adjusted */
-    if (f->zero && !f->minus)
-        for (; padding > 0; padding--)
-            count += _putchar('0');
-
-    /* Print number in correct order */
-    while (i--)
-        count += _putchar(buffer[i]);
-
-    /* If left-adjusted, add spaces after */
-    if (f->minus)
-        for (; padding > 0; padding--)
+    if (!f->minus && !f->zero)
+	{
+        while (padding-- > 0)
+		{
             count += _putchar(' ');
+		}
+	}
 
-    return count;
+	if (!f->minus && f->zero)
+	{
+        while (padding-- > 0)
+		{
+            count += _putchar('0');
+		}
+	}
+	while (i--)
+	{
+		count += _putchar(buffer[i]);
+	}
+	if (f->minus)
+	{
+		while (padding-- > 0)
+		{
+			count += _putchar(' ');
+		}
+	}
+	return count;
 }
 
 int print_hex_lower(va_list args, format_flags_t *f)
@@ -53,4 +64,3 @@ int print_hex_upper(va_list args, format_flags_t *f)
 {
     return print_hex(args, f, 1);
 }
-
